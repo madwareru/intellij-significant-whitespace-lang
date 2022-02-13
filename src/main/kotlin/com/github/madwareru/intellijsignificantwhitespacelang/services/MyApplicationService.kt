b166633d5a6ad9c81ed9421dbd8f07e638ed7337
GitHub Action <action@github.com>
2022-02-13 21:06:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/madwareru/intellijsignificantwhitespacelang/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.madwareru.intellijsignificantwhitespacelang.services
+
+import com.github.madwareru.intellijsignificantwhitespacelang.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/madwareru/intellijsignificantwhitespacelang/services/MyProjectService.kt
+++ src/main/kotlin/com/github/madwareru/intellijsignificantwhitespacelang/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.madwareru.intellijsignificantwhitespacelang.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.madwareru.intellijsignificantwhitespacelang.MyBundle
 
 class MyProjectService(project: Project) {
 
